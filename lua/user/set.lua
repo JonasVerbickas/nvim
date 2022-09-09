b9c3fa80a4b6160a45d4fcb9408b00e9be888598
JonasVerbickas <jonasverbic@gmail.com>
2022-09-09 19:19:57 +0300

    Increase update time

--- lua/user/set.lua
+++ lua/user/set.lua
@@ -33,7 +33,7 @@ vim.opt.cmdheight = 1
 
 -- Having longer updatetime (default is 4000 ms = 4 s) leads to noticeable
 -- delays and poor user experience.
-vim.opt.updatetime = 100
+vim.opt.updatetime = 300
 
 -- Don't pass messages to |ins-completion-menu|.
 vim.opt.shortmess:append("c")
